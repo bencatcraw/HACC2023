615b5ee019dd077abd15dea9dff68ab5bf08671c
Mairi Yoshioka <yoshiokamairi@gmail.com>
2023-11-07 19:51:48 -1000

    all facilities page linked to individual websites but background janky

--- app/imports/ui/pages/AllFacilities.jsx
+++ app/imports/ui/pages/AllFacilities.jsx
@@ -3,22 +3,197 @@ import { Container, Image } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const AllFacilities = () => (
-  <Container id="all-facilities-page" fluid className="py-3">
+  <Container id="all-facilities-page" fluid className="py-3 facilities">
     <div className="text-center">
-      <Image roundedCircle src="/images/meteor-logo.png" height="300px" />
+      <Image src="https://thumbs.dreamstime.com/b/medical-team-isolated-white-set-hospital-staff-doctors-nurses-surgeon-healthcare-concept-cartoon-83640952.jpg" height="200px" />
     </div>
     <Container color="#97C1D8">
       <ul className="justify-content-start">
-        <li className="fs-4">Fresenius Medical Care - Lanai Community Dialysis Center</li>
-        <li className="fs-4">Liberty Dialysis - Hawaii LLC - Ala Moana Dialysis Facility</li>
-        <li className="fs-4">Liberty Dialysis - Hawaii LLC - Hilo Dialysis Facility</li>
-        <li className="fs-4">Liberty Dialysis - Hawaii LLC - Kauai Dialysis Facility</li>
-        <li className="fs-4">Liberty Dialysis - Hawaii LLC - Hawaii Kai Dialysis Facility</li>
-        <li className="fs-4">Adventist Health Castle Home Care</li>
-        <li className="fs-4">Cradles N’ Crayons, LLC (Kahului)</li>
-        <li className="fs-4">Hale Makua Home Health Care Agency</li>
-        <li className="fs-4">Hilo Medical Center Home Care</li>
-        <li className="fs-4">Kohala Home Health Care of North Hawaii Community Hospital</li>
+        <li className="fs-4">
+          <a href="https://carelistings.com/rural-health-centers/laie-hi/castle-health-clinic-of-laie/5ace880c93efd2372f97b93a">
+            Castle Health Clinic of Laie
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.easthawaiihealthclinics.org/primary-care-1190/">
+            East Hawaii Health Clinic at 1190 Waianuenue
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://kauai.hhsc.org/facilities/the-specialty-clinic-at-kalaheo/">
+            HHSC-KAUAI REGION CLINICS SPECIALTY CLINIC AT KALAHEO
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://healthy.kaiserpermanente.org/hawaii/facilities/kaiser-permanente-kihei-clinic-100440">
+            Kihei Clinic
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.queens.org/locations/hospitals/molokai/">
+            Molokai General Hospital Rural Health Clinic
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.alohanursing.com/">
+            ALOHA NURSING & REHAB CENTRE
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.ohanapacific.com/ann-pearl">
+            ANN PEARL NURSING FACILITY
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.halehoolahamakua.org/">
+            HALE HO’OLA HAMAKUA
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.ohanapacific.com/hale-kupuna-rehab-nursing">
+            HALE KUPUNA HERITAGE HOME, LLC
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.ccoh.us/">
+            THE CARE CENTER OF HONOLULU
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://pacificmobileimaging.com/">
+            ALOHA MOBILE IMAGING
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://hawaii.salvationarmy.org/hawaii_2/adhs/">
+            SALVATION ARMY ADULT DAY HEALTH SERVICES
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.yukiookutsustateveteranshome.org/">
+            YUKIO OKUTSU STATE VETERANS HOME
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.ohanapacific.com/kauai-adult-day-health-center">
+            KAUAI ADULT DAY HEALTH CENTER
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://alohaeye.com/">
+            ALOHA EYE CLINIC SURGICAL CENTER, LLC
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.drshimching.com/">
+            ASIA PACIFIC SURGERY, LLC
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.bigislandgastro.com/">
+            BIG ISLAND ENDOSCOPY CENTER, LLC
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.freseniuskidneycare.com/dialysis-centers/hawaii/2809">
+            Fresenius Medical Care - Lanai Community Dialysis Center
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.freseniuskidneycare.com/dialysis-centers/hawaii/100115">
+            Liberty Dialysis - Hawaii LLC - Ala Moana Dialysis Facility
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.freseniuskidneycare.com/dialysis-centers/hawaii/7383">
+            Liberty Dialysis - Hawaii LLC - Hilo Dialysis Facility
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.freseniuskidneycare.com/dialysis-centers/hawaii/7376">
+            Liberty Dialysis - Hawaii LLC - Kauai Dialysis Facility
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.adventisthealth.org/home-care-services/locations/castle/">
+            Adventist Health Castle Home Care
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://cradlesncrayons.com/">
+            Cradles N’ Crayons, LLC (Kahului)
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.halemakua.org/">
+            Hale Makua Home Health Care Agency
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.hilomedicalcenter.org/">
+            Hilo Medical Center Home Care
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.queens.org/locations/hospitals/north-hawaii/services/kohala-home/">
+            Kohala Home Health Care of North Hawaii Community Hospital
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://bristolhospice.com/hawaii/">
+            BRISTOL HOSPICE – HAWAII, LLC
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.hospicemaui.org/">
+            HOSPICE MAUI
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.hawaiicarechoices.org/">
+            HOSPICE OF HILO
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.hawaiipacifichealth.org/kapiolani/">
+            KAPIOLANI MEDICAL CENTER for WOMEN and CHILDREN
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://healthy.kaiserpermanente.org/hawaii/facilities/kaiser-permanente-moanalua-medical-center-100434">
+            KAISER FOUNDATION HOSPITAL
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.kuakini.org/wps/portal/public/About-Us/Subsidiaries/Kuakini-Medical-Center">
+            KUAKINI MEDICAL CENTER
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.ori-hawaii.com/">
+            OPPORTUNITIES AND RESOURCES, INC.
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.mauihealth.org/locations/kula-hospital/">
+            KULA HOSPITAL
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://www.thearcinhawaii.org/">
+            KUAKINI MEDICAL CENTER
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://legacyoflifehawaii.org/">
+            LEGACY OF LIFE HAWAII
+          </a>
+        </li>
+        <li className="fs-4">
+          <a href="https://pacificmobileimaging.com/">
+            PACIFIC MOBILE IMAGING, LLC
+          </a>
+        </li>
       </ul>
     </Container>
   </Container>
